8b5d4a88e43b37629a41cb734da0c19ccdf3548b
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-04-04 16:58:55 +0200

    Prepare the Settings activity (not active yet).

--- PerfectTunerAndroid/src/com/harmoneye/tuner/activity/TunerActivity.java
+++ PerfectTunerAndroid/src/com/harmoneye/tuner/activity/TunerActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
 
 import com.harmoneye.tuner.R;
 import com.harmoneye.tuner.analysis.AnalysisThread;
@@ -79,6 +78,37 @@ public class TunerActivity extends Activity {
 			analysisThread.interrupt();
 		}
 	}
+
+	/*
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			openSettings();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void openSettings() {
+		Intent intent = new Intent(this, SettingsActivity.class);
+		startActivity(intent);
+	}
+
+	
+	*/
+	
 	private void enableFullScreen() {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
