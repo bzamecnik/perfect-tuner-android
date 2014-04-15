bd1a6f59c8a178e91e9883e4aefd5fa32b0b6027
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-04-15 17:05:23 +0200

    Move the "keep screen on" flag to the XML config.

--- perfect-tuner-android/src/com/harmoneye/tuner/activity/TunerActivity.java
+++ perfect-tuner-android/src/com/harmoneye/tuner/activity/TunerActivity.java
@@ -132,14 +132,11 @@ public class TunerActivity extends Activity {
 		startActivity(intent);
 	}
 
-	
 	*/
-	
+
 	private void enableFullScreen() {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 			WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	}
-	
 }
